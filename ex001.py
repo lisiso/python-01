0059e41645aeec32614d51e3a00bb4689fc1f6fe
Alexey <li-si-so@yandex.ru>
2016-03-11 07:31:25 +0300

    Переписали программу, теперь это пример из учебника

--- ex001.py
+++ ex001.py
@@ -1,3 +1,11 @@
 #!
-print("первая строчка")
-print("еще одна строчка")
+def get_int(msg):
+    while True:
+        try:
+            i = int(input(msg))
+            return i
+        except ValueError as err:
+            print(err)
+
+age = get_int("enter your age: ")
+print("Your age = %d" % age)
