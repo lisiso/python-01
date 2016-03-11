6eae882f05927c86dcaae323ecf299b1811c125b
Alexey <li-si-so@yandex.ru>
2016-03-11 08:31:45 +0300

    Пример БОЛЬШИЕ ЦИФРЫ

--- ex001.py
+++ ex001.py
@@ -1,11 +1,40 @@
 #!
-def get_int(msg):
-    while True:
-        try:
-            i = int(input(msg))
-            return i
-        except ValueError as err:
-            print(err)
+import sys
 
-age = get_int("enter your age: ")
-print("Your age = %d" % age)
+Zero = ["  ***  ",
+        " *   * ",
+        "*     *",
+        "*     *",
+        "*     *",
+        " *   * ",
+        "  ***  "]
+One = [" * ", "** ", " * ", " * ", " * ", " * ", "***"]
+Two = [" *** ", "*   *", "*  * ", "  *  ", " *   ", "*    ", "*****"]
+Three = [" *** ", "*   *", "    *", "  ** ", "    *", "*   *", " *** "]
+Four = ["   *  ", "  **  ", " * *  ", "*  *  ", "******", "   *  ", "   *  "]
+Five = ["*****", "*    ", "*    ", " *** ", "    *", "*   *", " *** "]
+Six = [" *** ", "*    ", "*    ", "**** ", "*   *", "*   *", " *** "]
+Seven = ["*****", "    *", "   * ", "  *  ", " *   ", "*    ", "*    "]
+Eight = [" *** ", "*   *", "*   *", " *** ", "*   *", "*   *", " *** "]
+Nine = [" ****", "*   *", "*   *", " ****", "    *", "    *", "    *"]
+
+Digits = [Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine]
+
+try:
+    digits = sys.argv[1]
+    row = 0
+    while row < 7:
+        line = ""
+        column = 0
+        while column < len(digits):
+            number = int(digits[column])
+            digit = Digits[number]
+            line += digit[row] + "  "
+            column += 1
+        print(line)
+        row += 1
+except IndexError:
+    print("usage: bigdigits.py <number>")
+except ValueError as err:
+    print(err, "in", digits)
+
